9d8c5940b75923c1fa81d103e17c36b1d2db4606
KyuSeungSim <66705345+KyuSeungSim@users.noreply.github.com>
2021-04-16 19:12:16 -0500

    Update Scheduling.h

--- Scheduling.h
+++ Scheduling.h
@@ -1,9 +1,32 @@
+#pragma once
 #ifndef SCHEDULING_H
 #define SCHEDULING_H
+using namespace std;
+
 class Scheduling
 {
 private:
+	int busType;			// 1 = LuxuryBus, 2 = MiniBus, 3 = MiniBan
+	int luxuryBus;
+	int miniBus;
+	int miniBan;
+	int busTotal;
+	int activeLuxuryBus;
+	int activeMiniBus;
+	int activeMiniBan;
+	int activeTotalBus;
+	int activeState;		// 0 = No active, 1 = active
 public:
-};
+	Scheduling(int, int, int, int);
+	string getBusType(int);
+	int getActiveLuxuryBus();
+	int getActiveMiniBus();
+	int getActiveMiniBan();
 
+	void weekCheck(int);
+	bool minimumCheck(int);
+	void demandCheck(int);
+	void reservationCheck();
+	void busListCheck();
+};
 #endif
